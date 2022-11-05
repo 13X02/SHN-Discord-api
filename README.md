27e76b2564458f7990e211449c0d9da4bf463e84
Abhijith H <abhijithh496@gmail.com>
2022-11-06 00:23:16 +0530

    new features added

--- README.md
+++ README.md
@@ -2,18 +2,24 @@ The Bot manipulates image input according to the users conditions
 
 
 
-Current Features:
-    Background Remover
+##Current Features and their commands:
 
-Upcoming Features:
-    Image Filters
+$removebg <image url> - Removes background from image 
+$blur <image url> - Blurs the image 
+$grayscale <image url> - Converts image to grayscale 
+$invert <image url> - Inverts the image 
+$enhance <image url> - Enhances the image 
+$flip <image url> - Flips the image 
+$mirror <image url> - Mirrors the image 
+$rotate <image url> - Rotates the image
+
+##Upcoming Features:
     Image Resizing
 
 
 
-Walkthrough video https://drive.google.com/file/d/1fIpj8GQM_MLehl_dP5Jb08QlbsPd0jie/view?usp=share_link
+Walkthrough video https://drive.google.com/file/d/1TJK8NbWrdYfGneJAV3aqvx5Flv14m-SW/view?usp=sharing
 
 
-
-api used : Discord api
-           removebg api
+##api used : Discord api
+             removebg api
--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import discord
 import os
 import requests
 from dotenv import load_dotenv
+from PIL import Image,ImageFilter  
+from PIL import ImageEnhance  , ImageOps
+
 
 load_dotenv()
 
@@ -44,6 +47,97 @@ async def on_message(message):
         else:
             await message.channel.send("Invalid Image format ")
             print("Error:", response.status_code, response.text)
+    elif message.content.startswith('$blur'):
+        if not message.attachments:
+            url = message.content[9:]           
+        else:
+            url = message.attachments[0].url
+        response = requests.get(url, stream=True)
+        if response.status_code == 200:
+            response.raw.decode_content = True
+            img = Image.open(response.raw)
+            img = img.filter(ImageFilter.GaussianBlur(radius=10))
+            img.save('blurred.png')
+            await message.channel.send(file=discord.File('blurred.png'))
+        else:
+            await message.channel.send("Invalid Image format ")
+            print("Error:", response.status_code, response.text)
+    elif message.content.startswith('$help'):
+        await message.channel.send("```$removebg <image url> - Removes background from image \n$blur <image url> - Blurs the image \n$grayscale <image url> - Converts image to grayscale \n$invert <image url> - Inverts the image \n$enhance <image url> - Enhances the image \n$flip <image url> - Flips the image \n$mirror <image url> - Mirrors the image \n$rotate <image url> - Rotates the image```")
+    elif message.content.startswith('$grayscale'):
+        if not message.attachments:
+            url = message.content[9:]           
+        else:
+            url = message.attachments[0].url
+        response = requests.get(url, stream=True)
+        if response.status_code == 200:
+            response.raw.decode_content = True
+            img = Image.open(response.raw)
+            gray_image = ImageOps.grayscale(img)
+            gray_image.save('gray.png')
+            await message.channel.send(file=discord.File('gray.png'))
+    elif message.content.startswith('$invert'):
+        if not message.attachments:
+            url = message.content[9:]           
+        else:
+            url = message.attachments[0].url
+        response = requests.get(url, stream=True)
+        if response.status_code == 200:
+            response.raw.decode_content = True
+            img = Image.open(response.raw)
+            inverted_image = ImageOps.invert(img)
+            inverted_image.save('inverted.png')
+            await message.channel.send(file=discord.File('inverted.png'))
+    elif message.content.startswith('$enhance'):
+        if not message.attachments:
+            url = message.content[9:]           
+        else:
+            url = message.attachments[0].url
+        response = requests.get(url, stream=True)
+        if response.status_code == 200:
+            response.raw.decode_content = True
+            img = Image.open(response.raw)
+            enhancer = ImageEnhance.Contrast(img)
+            enhanced_image = enhancer.enhance(2)
+            enhanced_image.save('enhanced.png')
+            await message.channel.send(file=discord.File('enhanced.png'))
+    elif message.content.startswith('$flip'):
+        if not message.attachments:
+            url = message.content[9:]           
+        else:
+            url = message.attachments[0].url
+        response = requests.get(url, stream=True)
+        if response.status_code == 200:
+            response.raw.decode_content = True
+            img = Image.open(response.raw)
+            flipped_image = ImageOps.flip(img)
+            flipped_image.save('flipped.png')
+            await message.channel.send(file=discord.File('flipped.png'))
+    elif message.content.startswith('$mirror'):
+        if not message.attachments:
+            url = message.content[9:]           
+        else:
+            url = message.attachments[0].url
+        response = requests.get(url, stream=True)
+        if response.status_code == 200:
+            response.raw.decode_content = True
+            img = Image.open(response.raw)
+            mirrored_image = ImageOps.mirror(img)
+            mirrored_image.save('mirrored.png')
+            await message.channel.send(file=discord.File('mirrored.png'))
+    elif message.content.startswith('$rotate'):
+        if not message.attachments:
+            url = message.content[9:]           
+        else:
+            url = message.attachments[0].url
+        response = requests.get(url, stream=True)
+        if response.status_code == 200:
+            response.raw.decode_content = True
+            img = Image.open(response.raw)
+            rotated_image = img.rotate(90)
+            rotated_image.save('rotated.png')
+            await message.channel.send(file=discord.File('rotated.png'))
+    
 
 client.run(os.getenv('TOKEN'))
 
